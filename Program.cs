1f9420ecff5d8db346713f346983df50f20071d8
StevenJV <svore@navexglobal.com>
2022-08-03 12:16:59 -0400

    I say hello

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello");
+var names = new List<string>() { "John", "George", "Paul", "Ringo" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+Console.WriteLine("I say hello, you say goodbye.");
+
