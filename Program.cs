5a9d389c2860410650bd07a2d04b8d3d35479992
StevenJV <svore@navexglobal.com>
2022-08-03 15:33:59 -0400

    goodbye

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello");
-var names = new List<string>() { "John", "George", "Paul", "Ringo" };
+var names = new List<string>() { "Billie Joe", "Mike", "Tre"};
 foreach (string name in names)
 {
     Console.WriteLine(name);
 }
-Console.WriteLine("I say hello, you say goodbye.");
+Console.WriteLine("Say goodbye to the ones that we love.");
 
